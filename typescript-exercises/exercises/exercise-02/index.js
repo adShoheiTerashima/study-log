10ab076a4e596b6c41359afb0c9d5911f4d451d4
adShoheiTerashima <s-terashima@adventure-inc.co.jp>
2022-01-06 19:06:41 +0900

    feat(te): exercise02

---
+++ typescript-exercises/exercises/exercise-02/index.js
@@ -0,0 +1,30 @@
+"use strict";
+exports.__esModule = true;
+exports.logPerson = exports.persons = void 0;
+exports.persons = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+function logPerson(user) {
+    console.log(" - ".concat(user.name, ", ").concat(user.age));
+}
+exports.logPerson = logPerson;
+exports.persons.forEach(logPerson);
