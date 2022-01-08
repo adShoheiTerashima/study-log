9cdec9484327791e4738af46f32f703300da2f7b
adShoheiTerashima <s-terashima@adventure-inc.co.jp>
2022-01-08 21:00:48 +0900

    feat(te): exercise04 and apply lint

--- typescript-exercises/exercises/exercise-03/index.ts
+++ typescript-exercises/exercises/exercise-03/index.ts
@@ -1,48 +1,48 @@
 interface User {
-    name: string;
-    age: number;
-    occupation: string;
+  name: string;
+  age: number;
+  occupation: string;
 }
 
 interface Admin {
-    name: string;
-    age: number;
-    role: string;
+  name: string;
+  age: number;
+  role: string;
 }
 
 export type Person = User | Admin;
 
 export const persons: Person[] = [
-    {
-        name: 'Max Mustermann',
-        age: 25,
-        occupation: 'Chimney sweep'
-    },
-    {
-        name: 'Jane Doe',
-        age: 32,
-        role: 'Administrator'
-    },
-    {
-        name: 'Kate Müller',
-        age: 23,
-        occupation: 'Astronaut'
-    },
-    {
-        name: 'Bruce Willis',
-        age: 64,
-        role: 'World saver'
-    }
+  {
+    name: "Max Mustermann",
+    age: 25,
+    occupation: "Chimney sweep",
+  },
+  {
+    name: "Jane Doe",
+    age: 32,
+    role: "Administrator",
+  },
+  {
+    name: "Kate Müller",
+    age: 23,
+    occupation: "Astronaut",
+  },
+  {
+    name: "Bruce Willis",
+    age: 64,
+    role: "World saver",
+  },
 ];
 
 export function logPerson(person: Person) {
-    let additionalInformation: string;
-    if ('role' in person) {
-        additionalInformation = person.role;
-    } else {
-        additionalInformation = person.occupation;
-    }
-    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+  let additionalInformation: string;
+  if ("role" in person) {
+    additionalInformation = person.role;
+  } else {
+    additionalInformation = person.occupation;
+  }
+  console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
 }
 
 persons.forEach(logPerson);
